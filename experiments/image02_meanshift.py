fb84b9e4317164aac4f4ebb94882d896767d39b4
jbfasquel <nom2@email.com>
2016-05-04 15:41:35 +0200

    - Image02: Update of truth (residues instead of filled regions) - Recomputing of experimental results for image02

--- experiments/image02_meanshift.py
+++ experiments/image02_meanshift.py
@@ -13,17 +13,16 @@ p_desc="tumor<liver<vessel"
 
 # LOAD DATA
 image=np.load(truth_dir+"image_filtered.npy")
-roi=np.load(truth_dir+"region_liver.npy")
+roi=np.load(truth_dir+"roi.npy")
 
-'''
+
 # SEGMENTATION MEANSHIFT
 label=skgti.utils.mean_shift(image,15,roi,False,True) #0.1 OK
 # RECOGNITION
 id2r,matcher=skgti.core.recognize_regions(image,label,t_desc,p_desc,roi=roi,manage_bounds=True,thickness=2,filtering=True,verbose=True)
-skgti.io.pickle_matcher(matcher,save_dir+"matcher.pkl")
 skgti.io.save_matcher_details(matcher,image,label,roi,save_dir,True,slices=[45])
-'''
-matcher=skgti.io.unpickle_matcher(save_dir+"matcher.pkl")
+skgti.io.pickle_matcher(matcher,save_dir+"matcher.pkl")
+#matcher=skgti.io.unpickle_matcher(save_dir+"matcher.pkl")
 # EVALUATION VS TRUTH
 import helper
 classif,region2sim=helper.compared_with_truth(image,t_desc,p_desc,truth_dir,save_dir+"06_final",save_dir+"07_eval_classif/",slices=[45])
--- experiments/image02_preparation.py
+++ experiments/image02_preparation.py
@@ -7,6 +7,7 @@ import skgtimage as skgti
 ######################################
 #CROP
 ######################################
+'''
 input="Database/image02/rawdata_nocrop/"
 output="Database/image02/truth/"
 
@@ -22,3 +23,21 @@ for i in ['liver','vessel','tumor']:
     image=np.load(os.path.join(input,"region_"+i+".npy"))
     image=skgti.utils.extract_subarray(image,ref_roi)
     np.save(os.path.join(output,"region_"+i+".npy"),image)
+'''
+
+
+input="Database/image02/truth_filled/"
+output="Database/image02/truth/"
+
+liver=np.load(os.path.join(input,"region_liver.npy")).astype(np.uint8)
+np.save(os.path.join(output,"roi.npy"),liver)
+tumors=np.load(os.path.join(input,"region_tumor.npy")).astype(np.uint8)
+vessels=255*(np.load(os.path.join(input,"region_vessel.npy")).astype(np.uint8))
+print(np.min(tumors),np.max(tumors))
+print(np.min(vessels),np.max(vessels))
+print(np.min(liver),np.max(liver))
+liver=liver-tumors-vessels
+np.save(os.path.join(output,"region_liver.npy"),liver)
+np.save(os.path.join(output,"region_vessel.npy"),vessels)
+np.save(os.path.join(output,"region_tumor.npy"),tumors)
+
